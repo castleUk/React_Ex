d3e701b103a0ad4a4f20f3dd233649869874344e
uk <ppkk24@naver.com>
2022-11-27 20:33:20 +0900

    1127

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
-import Expenses from "./components/Expenses";
 
-function App() {
+import NewExpense from './components/NewExpense/NewExpense';
+import Expenses from './components/expenses/Expenses';
+import React from 'react';
+
+const App = () => {
   const expenses = [
     {
       id: 'e1',
@@ -22,9 +25,27 @@ function App() {
       date: new Date(2021, 5, 12),
     },
   ];
+
+
+  const addExpenseHandler = expense => {
+    console.log('IN app.js');
+    console.log(expense);
+
+  };
+
+  // 예전의 리액트에서 사용하던 구문
+  // return React.createElement( // 가지 요소를 가짐 첫번째인자는 생성해야하는 요소, 두번째는 이런 요소를 설정하는 객체, 세번쨰는 컨텐츠, 
+  //   'div', 
+  //   {},
+  //   React.createElement('h2', {}, "Let's get started!" ),
+  //   React.createElement(Expenses, {items: expenses})
+  // );
+
+
+
   return (
     <div>
-      <h2>Let's get started!</h2>
+      <NewExpense onAddExpense={addExpenseHandler}/>
       <Expenses items={expenses}/>
     </div>
   );
