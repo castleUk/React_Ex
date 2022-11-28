2bbd5a6758cccb4ef09f08b90a62a6e33d435b4e
TakCK <117065720+TakCK@users.noreply.github.com>
2022-11-28 11:13:12 +0900

    First project final

--- src/components/expenses/ExpenseItem.js
+++ src/components/expenses/ExpenseItem.js
@@ -1,33 +1,20 @@
-import './ExpenseItem.css';
-import ExpenseDate from './ExpenseDate';
-import Card from '../UI/Card';
-import React, { useState } from "react";
+import "./ExpenseItem.css";
+import ExpenseDate from "./ExpenseDate";
+import Card from "../UI/Card";
+import React from "react";
 
 const ExpenseItem = (props) => {
-    // document.getElementById('root').addEventListener() 일반 자바스크립트의 명령형 방식
-    const [title, setTitle] = useState(props.title);
-    console.log('ExpenseItem evaluated by React');
-    // useState의 첫번째 값은 현재 상태값이고, 두번째값은 업데이트 될 값
-    
-
-    const clickHandler = () => {
-        setTitle('Updated!');
-        console.log('title')
-    };
-
-    return(
-        <Card className='expense-item'>
-            <ExpenseDate date={props.date}/>
-            <div className='expense-item__description'>
-                <h2>{title}</h2>
-                <div className='expense-item__price'>${props.amount}</div>
-            </div>
-            <button onClick={clickHandler}>Change Title</button>
-        </Card>
-    );
-
-
-
-}
+  return (
+    <li>
+      <Card className="expense-item">
+        <ExpenseDate date={props.date} />
+        <div className="expense-item__description">
+          <h2>{props.title}</h2>
+          <div className="expense-item__price">${props.amount}</div>
+        </div>
+      </Card>
+    </li>
+  );
+};
 
 export default ExpenseItem;
